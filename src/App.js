51030c571d850f2f339b10a728dab47c8efe8c18
Yi Chen <yi717123@ucf.edu>
2020-02-19 11:23:13 -0500

    Co-authored-by: thompson.taylor97@Knights.ucf.edu <thompson.taylor97@Knights.ucf.edu>

--- src/App.js
+++ src/App.js
@@ -2,10 +2,38 @@ import React from 'react';
 import './App.css';
 import Game from './components/TicTacToe/index.js';
 
-function App() {
-  return (
-    <Game />
-  );
+class Login extends  React.Component{
+  clickHandler() {
+    console.log("User is logging on");
+    console.log(this);
+  }
+  render() {
+  return <div>
+    <input type="text"/>
+    <input type="text"/>
+    <input type="button" value="Login" onClick={() => this.clickHandler()} />
+  </div>;
+  }
+}
+
+class App extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {loggedIn:false}
+  }
+  
+  render() {
+    let returnComponent;
+    const login = <Login />;
+    const game = <Game />;
+    if(!this.state.loggedIn){
+      returnComponent = login;
+    }
+    else{
+      returnComponent = game;
+    }
+    return returnComponent;
+  }
 }
 
 export default App;
