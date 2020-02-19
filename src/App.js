660919cd6770252632b63c9659e82214c34e5869
Yi Chen <yi717123@ucf.edu>
2020-02-19 11:52:27 -0500

    ice 9

--- src/App.js
+++ src/App.js
@@ -2,34 +2,55 @@ import React from 'react';
 import './App.css';
 import Game from './components/TicTacToe/index.js';
 
-class Login extends  React.Component{
-  clickHandler() {
+class Login extends  React.Component
+{
+  clickHandler() 
+  {
     console.log("User is logging on");
     console.log(this);
+    
+    for(const user of this.props.users) 
+    {
+      if(this.refs.username.value === user.username && 
+        this.refs.password.value === user.password)
+        this.props.loginUser();
+    }
   }
-  render() {
+  render() 
+  {
   return <div>
-    <input type="text"/>
-    <input type="text"/>
+    <input ref="username" type="text"/>
+    <input ref="password" type="text"/>
     <input type="button" value="Login" onClick={() => this.clickHandler()} />
   </div>;
   }
 }
 
-class App extends React.Component {
-  constructor(props){
+class App extends React.Component 
+{
+  constructor(props)
+  {
     super(props);
-    this.state = {loggedIn:false}
+    this.state = {loggedIn:false,
+      users:[{username:"test",password:"test"}]
+    }
   }
-  
-  render() {
+  logInUser(loggedInStatus)
+  {
+    this.setState({loggedIn:loggedInStatus});
+  }
+  render() 
+  {
     let returnComponent;
-    const login = <Login />;
-    const game = <Game />;
-    if(!this.state.loggedIn){
+    const login = <Login users={this.state.users}
+      loginUser={() => this.logInUser(true)}/>;
+    const game = <Game logOutUser={() => this.logInUser(false)} />;
+    if(!this.state.loggedIn)
+    {
       returnComponent = login;
     }
-    else{
+    else
+    {
       returnComponent = game;
     }
     return returnComponent;
