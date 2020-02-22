d2329ff71e95ed716e581416af3b3ab0427bc430
y604499781 <54754052+y604499781@users.noreply.github.com>
2020-02-21 22:22:37 -0500

    Updates

---
+++ precache-manifest.a4137c6b01eb14649ad1d54f3750f4be.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "578186de1fcd6d18b89f01ce1d25658c",
+    "url": "/react-tictactoe-app/index.html"
+  },
+  {
+    "revision": "e4719960bd4555382512",
+    "url": "/react-tictactoe-app/static/css/main.57eca423.chunk.css"
+  },
+  {
+    "revision": "ad97abc797f783333447",
+    "url": "/react-tictactoe-app/static/js/2.78901575.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/react-tictactoe-app/static/js/2.78901575.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e4719960bd4555382512",
+    "url": "/react-tictactoe-app/static/js/main.fba560d5.chunk.js"
+  },
+  {
+    "revision": "298d1991bc8af3def2e2",
+    "url": "/react-tictactoe-app/static/js/runtime-main.50814b03.js"
+  }
+]);
